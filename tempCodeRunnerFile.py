fe8f3c5f362017c51619a4648406f24238f31cf5
UmangBurgundy <umangjain1708@gmail.com>
2024-11-16 00:54:04 +0530

    initil commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+import pandas,numpy,shutil,os,random,functools,time,threading
+from multiprocessing import process,cpu_count
+from tkinter import *
+from tkinter import colorchooser
+from tkinter import filedialog
+from tkinter import ttk
+
+window=Tk()
